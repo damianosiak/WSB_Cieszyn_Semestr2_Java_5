7eecc13ab79a89596f1fe39da282b01055c104b2
Damian Osiak <damianosiak34@gmail.com>
2020-04-03 19:24:55 +0200

    Initial commit

---
+++ src/main/java/com/damianosiak/semestr2/Semestr2Application.java
@@ -0,0 +1,13 @@
+package com.damianosiak.semestr2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Semestr2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Semestr2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/damianosiak/semestr2/Semestr2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.damianosiak.semestr2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Semestr2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
